417a1726c50bc4d5150425de633ed7bb7dbf769d
jaime-robertson1 <jaime-robertson@live.nmit.ac.nz>
2025-02-24 11:21:53 +1300

    New JS code

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 //console.log("Jaime Robertson")
 // Learning JS in SDV503 class
 /** multi- line text */
-console.log(2+1)
+//console.log(2+1)
+
+//ES5
+var a = 2
+
+//ES6
+const b = 3
+let c = 4           // use these over var
+
+//+,-,/ and *
+console.log(5-2)
+
+let num1 = 5
+let num2 = 10
+const result = num1 * num2
+console.log("result=",result)
+
+let num3 = 30
+let num4 = 4
+const outcome = num3 / num4
+console.log("result=", outcome)
